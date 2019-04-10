8b8ddbabcd1f74649bfb77d0bc9b2f543db94a78
alexandre ulas <alexandre.ulas@audience-plus.com>
2019-04-10 09:40:05 +0200

    first commit

---
+++ Rock-paper-scissorsTests/Rock_paper_scissorsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rock_paper_scissorsTests.swift
+//  Rock-paper-scissorsTests
+//
+//  Created by alexandre ulas on 10/04/2019.
+//  Copyright © 2019 Alexandre.ulas. All rights reserved.
+//
+
+import XCTest
+@testable import Rock_paper_scissors
+
+class Rock_paper_scissorsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
